33d2496f31e4166b5f91beef0c85d0d530f2ca9f
MatheusHBitencourt <matheus.svsg@gmail.com>
2025-04-18 17:35:10 -0300

    feat: add main modules

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: 'http://localhost:3001', // 👈 frontend
+    credentials: true, // 👈 permite cookies
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.use(bodyParser.json({ limit: '5mb' }));
+  app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
